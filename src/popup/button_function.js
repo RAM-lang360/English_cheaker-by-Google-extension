1a547db7a9824f4a3ba4d3a0a2fcc8a78999262b
RyoMoriya-1022 <eva1022moriya@gmail.com>
2025-06-05 15:50:30 +0900

    button_functionの削除

--- src/popup/button_function.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll(".order .order-btn");
-
-buttons.forEach(btn => {
-  btn.addEventListener("click", () => {
-    buttons.forEach(b => b.classList.remove("selected")); // 全部外す
-    btn.classList.add("selected"); // 押されたやつだけ selected
-    console.log("選択されたボタンID:", btn.id);
-  });
-});
